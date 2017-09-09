fdfcdca703ad2a37fbb4701da7b1d5e5e09eb813
Flavio Coutinho <fegemo@gmail.com>
2017-09-09 18:27:54 -0300

    Exercise 1: loading existent tasks

--- tarefas.js
+++ tarefas.js
@@ -10,3 +10,25 @@ let tarefas = [
     marcado: true
   }
 ];
+
+// Exercício 1: carregar as tarefas existentes
+// ------------
+//
+let containerEl = document.querySelector('#lista-tarefas');
+function insereTarefaNaPagina(tarefa) {
+  let tarefaLiEl = document.createElement('li');
+  tarefaLiEl.innerHTML = tarefa.nome;
+  tarefaLiEl.classList.add('item-tarefa');
+  if (tarefa.marcado) {
+    tarefaLiEl.classList.add('marcado');
+  }
+
+  containerEl.appendChild(tarefaLiEl);
+}
+
+
+// limpa a <ul> do que quer que esteja lá
+containerEl.innerHTML = '';
+// chama a função que insere na página para cada tarefa
+// do vetor 'tarefas'
+tarefas.forEach(insereTarefaNaPagina);
