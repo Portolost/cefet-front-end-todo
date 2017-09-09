64c5f25051b4f804a3051e60ed68b36941452f29
Flavio Coutinho <fegemo@gmail.com>
2017-09-09 18:31:00 -0300

    Exercise 2: adding a new task

--- tarefas.js
+++ tarefas.js
@@ -32,3 +32,32 @@ containerEl.innerHTML = '';
 // chama a função que insere na página para cada tarefa
 // do vetor 'tarefas'
 tarefas.forEach(insereTarefaNaPagina);
+
+
+
+
+
+// Exercício 2: incluir uma nova tarefa
+// -----------
+//
+let botaoIncluirEl = document.querySelector('#incluir-nova-tarefa');
+botaoIncluirEl.addEventListener('click', function(e) {
+  let nomeInputEl = document.querySelector('#nova-tarefa-nome');
+
+  // cria um novo objeto 'tarefa'
+  let novaTarefa = {
+    nome: nomeInputEl.value,
+    categoria: 'outros',
+    marcado: false
+  };
+
+  // coloca-o no vetor 'tarefas'
+  tarefas.push(novaTarefa);
+  // cria o <li> referente a essa tarefa e o
+  // insere na página
+  insereTarefaNaPagina(novaTarefa);
+
+  // limpar o input e devolver o foco pra ele
+  nomeInputEl.value = '';
+  nomeInputEl.focus();
+});
