fe9a676c68bd61be24438c8f69d4a5a14568164d
Gabriel Comonian Porto <74382419+Portolost@users.noreply.github.com>
2021-03-17 18:24:45 -0300

    17/03/2021

--- tarefas.js
+++ tarefas.js
@@ -10,3 +10,43 @@ let tarefas = [
     marcado: true
   }
 ];
+
+for (tarefa of tarefas){
+  insereTarefaNaPagina(tarefa);
+}
+
+function insereTarefaNaPagina(tarefa){
+  let listaEl = document.querySelector('#lista-tarefas');
+  let liEl = document.createElement('li');
+  let allLi = document.querySelector('.item-tarefa');
+  
+  listaEl.insertBefore(liEl, allLi);
+  liEl.innerHTML = tarefa.nome;
+  liEl.classList.add('item-tarefa');
+  liEl.classList.add('categoria-'+tarefa.categoria);
+  
+  if (tarefa.marcado === true)
+    liEl.classList.add('marcado');
+}
+function criarTarefa(e){
+  let novaTarefaIn = document.querySelector('#nova-tarefa-nome');
+  let novaCategoriaIn = document.querySelector('#nova-tarefa-categoria');
+  let tarefa = {
+    nome: novaTarefaIn.value,
+    categoria: novaCategoriaIn.options[novaCategoriaIn.selectedIndex].value,
+    marcado: false
+  }
+  tarefas.push(tarefa);
+  insereTarefaNaPagina(tarefa)
+  novaTarefaIn.value = '';
+}
+
+let botaoEl = document.querySelector('#incluir-nova-tarefa');
+
+botaoEl.addEventListener('click', criarTarefa);
+
+addEventListener('keyup', function(e){
+  if (e.key === 'Enter'){
+    criarTarefa();
+  }
+});
